cfca1c93320a9361f567fe6b196812ff836aca1c
yaya117788 <77266948+yaya117788@users.noreply.github.com>
2021-01-26 17:30:29 +0900

    Add files via upload

---
+++ 백준 Online Judge, 코딩도장 알고리즘 문제풀이/심사문제 27.6 파일관련.py
@@ -0,0 +1,13 @@
+with open('words.txt','r')as file:
+    line = None
+    line = file.readline()
+    lines = line.split()
+    a = []
+    c = []
+    for i in range(len(lines)):
+        if 'c' in lines[i]:
+            a.append(lines[i])
+    for i in a:
+        c.append(i.strip(',.'))
+    print('\n'.join(c))
+            
