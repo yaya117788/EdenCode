3f2a5672172facb45ad3fc3a1207f17e75a8857a
Eden Lee <yaya1177@hanmail.net>
2021-04-14 00:57:11 +0900

    example

---
+++ JavaScript Practice/ex001.py
@@ -0,0 +1,16 @@
+#it's a practice
+kor=77.5
+print(type(kor))
+
+eng=float(input('영어점수를 입력해주세요'))
+print(eng)
+print(type(eng))
+
+thai= '홍길동'
+jap= int(772.3)
+print(type(thai))
+print(type(jap))
+print(jap)
+
+
+
